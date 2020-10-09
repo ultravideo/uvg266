5a6806cbf720f89fc88550ab3422504f452973e4
Marko Viitanen <fador@iki.fi>
2020-10-09 09:37:15 +0300

    [CI] Limit testing parameters to those that work

--- tests/test_weird_shapes.sh
+++ tests/test_weird_shapes.sh
@@ -3,6 +3,6 @@
 set -eu
 . "${0%/*}/util.sh"
 
-valgrind_test  16x16  10 --threads=2 --owf=1 --preset=veryslow
-valgrind_test 256x16  10 --threads=2 --owf=1 --preset=veryslow
-valgrind_test  16x256 10 --threads=2 --owf=1 --preset=veryslow
+valgrind_test  16x16  10 --threads=0 --no-wpp --preset=veryslow
+valgrind_test 256x16  10 --threads=0 --no-wpp --preset=veryslow
+valgrind_test  16x256 10 --threads=0 --no-wpp --preset=veryslow
