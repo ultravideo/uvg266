2bd9bcdf28a3aefcdcb22629343aa79e16221d94
Marko Viitanen <fador@iki.fi>
2020-10-13 14:06:59 +0300

    [CI] Use QP 37 on one intra test

--- tests/test_intra.sh
+++ tests/test_intra.sh
@@ -8,4 +8,4 @@ set -eu
 
 common_args='256x128 10 -p1 --preset=ultrafast --threads=0 --no-wpp --no-tmvp --no-deblock --sao=0 --cpuid=0'
 valgrind_test $common_args --rd=1
-valgrind_test $common_args --rd=2 --no-transform-skip
+valgrind_test $common_args --rd=2 --no-transform-skip --qp 37
