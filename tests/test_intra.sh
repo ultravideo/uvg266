42edbd031ff99ae8b560fc89d1c1af6b51acc495
Joose Sainio <joose.sainio@tuni.fi>
2021-04-23 08:28:36 +0000

    Add 4x4 CUs to tests

--- tests/test_intra.sh
+++ tests/test_intra.sh
@@ -6,9 +6,10 @@ set -eu
 
 . "${0%/*}/util.sh"
 
-common_args='256x128 10 -p1 --preset=ultrafast --threads=0 --no-wpp --no-tmvp --no-deblock --sao=0 --alf=full --cpuid=0'
+common_args='256x128 10 -p1 --preset=ultrafast --threads=0 --no-wpp --no-tmvp --no-deblock --sao=0 --alf=full --cpuid=0 --pu-depth-intra 0-4'
 valgrind_test $common_args --rd=1
 valgrind_test $common_args --rd=2 --no-transform-skip --qp 37
 valgrind_test $common_args --rd=2 --no-transform-skip --qp 37 --rdoq
 valgrind_test $common_args --rd=2 --no-transform-skip --qp 37 --signhide
 valgrind_test $common_args --rd=2 --no-transform-skip --qp 37 --signhide --rdoq
+valgrind_test $common_args --rd=2 --no-transform-skip --qp 37 --signhide --rdoq 
