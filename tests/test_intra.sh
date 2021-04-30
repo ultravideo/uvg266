aae8b829ac459142e1473b1ecd99f33b822759be
Joose Sainio <joose.sainio@tuni.fi>
2021-04-30 09:51:34 +0300

    [transform skip] Enable transform skip for tests that do not use rdoq

--- tests/test_intra.sh
+++ tests/test_intra.sh
@@ -8,8 +8,8 @@ set -eu
 
 common_args='256x128 10 yuv420p -p1 --preset=ultrafast --threads=0 --no-wpp --no-tmvp --no-deblock --sao=0 --alf=full --cpuid=0 --pu-depth-intra 0-4'
 valgrind_test $common_args --rd=1
-valgrind_test $common_args --rd=2 --no-transform-skip --qp 37
-valgrind_test $common_args --rd=2 --no-transform-skip --qp 37 --rdoq
-valgrind_test $common_args --rd=2 --no-transform-skip --qp 37 --signhide
+valgrind_test $common_args --rd=2 --transform-skip --qp 37
+valgrind_test $common_args --rd=2 --transform-skip --qp 37 --rdoq
+valgrind_test $common_args --rd=2 --transform-skip --qp 37 --signhide
 valgrind_test $common_args --rd=2 --no-transform-skip --qp 37 --signhide --rdoq
 valgrind_test $common_args --rd=2 --no-transform-skip --qp 37 --signhide --rdoq 
