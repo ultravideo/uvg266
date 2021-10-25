5b3d87f6c1a7f875b43efe601ddaa7434a2972ac
Marko Viitanen <fador@iki.fi>
2021-10-25 11:32:11 +0300

    [inter] Enable testing for inter tools

--- tests/test_tools.sh
+++ tests/test_tools.sh
@@ -5,12 +5,8 @@
 set -eu
 . "${0%/*}/util.sh"
 
-common_args='264x130 10 yuv420p -p0 -r1 --threads=2 --wpp --owf=1 --rd=0'
+common_args='264x130 10 yuv420p -p0 -r1 --threads=2 --wpp --owf=1 --rd=0 --pu-depth-inter 0-3'
 
 valgrind_test $common_args --no-rdoq --no-deblock --no-sao --no-signhide --subme=1 --pu-depth-intra=2-3
 valgrind_test $common_args --no-rdoq --no-signhide --subme=0
 valgrind_test $common_args --rdoq --no-deblock --no-sao --subme=0
-valgrind_test $common_args --vaq=8
-valgrind_test $common_args --vaq=8 --bitrate 3500
-valgrind_test $common_args --vaq=8 --rc-algorithm oba --bitrate 3500
-valgrind_test $common_args --lmcs
