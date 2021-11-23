d59c9f8246f9f5901563253665dba5d885674139
Marko Viitanen <fador@iki.fi>
2021-11-23 10:59:35 +0200

    [CI] Add bipred to tests

--- tests/test_tools.sh
+++ tests/test_tools.sh
@@ -10,4 +10,4 @@ common_args='264x128 10 yuv420p -p0 -r1 --threads=2 --wpp --owf=1 --rd=0 --pu-de
 valgrind_test $common_args --no-rdoq --no-deblock --no-sao --no-signhide --subme=1 --pu-depth-intra=2-3
 valgrind_test $common_args --no-rdoq --no-signhide --subme=0
 valgrind_test $common_args --rdoq --no-deblock --no-sao --subme=0
-valgrind_test $common_args --gop=8 --subme=4
+valgrind_test $common_args --gop=8 --subme=4 --bipred
