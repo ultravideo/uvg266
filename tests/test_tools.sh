36d302ba3897179473fd64b6d9f14c3506848038
Marko Viitanen <fador@iki.fi>
2021-11-26 17:04:30 +0200

    [bipred] Fix reference list writing when gop==0 and bipred are used, add to CI also

--- tests/test_tools.sh
+++ tests/test_tools.sh
@@ -8,6 +8,6 @@ set -eu
 common_args='264x128 10 yuv420p -p0 -r1 --threads=2 --wpp --owf=1 --rd=0 --pu-depth-inter 0-3 --no-bipred --no-tmvp --no-deblock --gop=0'
 
 valgrind_test $common_args --no-rdoq --no-deblock --no-sao --no-signhide --subme=1 --pu-depth-intra=2-3
-valgrind_test $common_args --no-rdoq --no-signhide --subme=0
+valgrind_test $common_args --no-rdoq --no-signhide --subme=0 --bipred
 valgrind_test $common_args --rdoq --no-deblock --no-sao --subme=0
 valgrind_test $common_args --gop=8 --subme=4 --bipred --tmvp
