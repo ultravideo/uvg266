9a67e622f53f83b410372812f6f5c8b01bbb3a47
Jaakko Laitinen <jaakko.laitinen@tuni.fi>
2021-12-10 15:18:39 +0200

    [CI] Enable Deblock in tests

--- tests/test_tools.sh
+++ tests/test_tools.sh
@@ -5,7 +5,7 @@
 set -eu
 . "${0%/*}/util.sh"
 
-common_args='264x128 10 yuv420p -p0 -r1 --threads=2 --wpp --owf=1 --rd=0 --pu-depth-inter 0-3 --no-bipred --no-tmvp --no-deblock --gop=0'
+common_args='264x128 10 yuv420p -p0 -r1 --threads=2 --wpp --owf=1 --rd=0 --pu-depth-inter 0-3 --no-bipred --no-tmvp --gop=0'
 
 valgrind_test $common_args --no-rdoq --no-deblock --no-sao --no-signhide --subme=1 --pu-depth-intra=2-3
 valgrind_test $common_args --no-rdoq --no-signhide --subme=0 --bipred
