c8539a3701f3b615e9a6015a533c08c0e65177de
Marko Viitanen <fador@iki.fi>
2022-05-04 19:54:42 +0300

    [intra] Limit intra depth to 3 (8x8) for now

--- tests/test_lmcs.sh
+++ tests/test_lmcs.sh
@@ -6,6 +6,6 @@ set -eu
 
 . "${0%/*}/util.sh"
 
-common_args='256x128 10 yuv420p -p1 --preset=ultrafast --threads=0 --no-wpp --no-tmvp --no-deblock --sao=0 --cpuid=0 --pu-depth-intra 0-4 --lmcs'
+common_args='256x128 10 yuv420p -p1 --preset=ultrafast --threads=0 --no-wpp --no-tmvp --no-deblock --sao=0 --cpuid=0 --pu-depth-intra 0-3 --lmcs'
 valgrind_test $common_args --deblock --sao=full --alf=full
 
