8223112cc110b431916ab5d79f7420982eb3de46
Joose Sainio <joose.sainio@tuni.fi>
2022-05-05 14:28:07 +0300

    Revert "[intra] Limit intra depth to 3 (8x8) for now"
    This reverts commit c8539a3701f3b615e9a6015a533c08c0e65177de.

--- src/global.h
+++ src/global.h
@@ -149,7 +149,7 @@ typedef int16_t mv_t;
 #define PU_DEPTH_INTER_MIN 0
 #define PU_DEPTH_INTER_MAX 3
 #define PU_DEPTH_INTRA_MIN 0
-#define PU_DEPTH_INTRA_MAX 3
+#define PU_DEPTH_INTRA_MAX 4
 
 //! Maximum number of layers in GOP structure (for allocating structures)
 #define MAX_GOP_LAYERS 6
