5fefea025f66cf170c2d11b3478cbec550ced8d4
Joose Sainio <joose.sainio@tuni.fi>
2022-06-29 16:35:55 +0300

    [lfnst] get constarints for jccr mode

--- src/search.c
+++ src/search.c
@@ -630,7 +630,7 @@ static double cu_rd_cost_tr_split_accurate(
       if (chroma_can_use_tr_skip) {
         CABAC_FBITS_UPDATE(cabac, &cabac->ctx.transform_skip_model_chroma, tr_cu->tr_skip & 2, tr_tree_bits, "transform_skip_flag");
       }
-      coeff_bits += uvg_get_coeff_cost(state, &lcu->coeff.joint_uv[index], NULL, chroma_width, COLOR_U, scan_order, 0);
+      coeff_bits += uvg_get_coeff_cost(state, &lcu->coeff.joint_uv[index], tr_cu, chroma_width, COLOR_U, scan_order, 0);
     }
   }
 
