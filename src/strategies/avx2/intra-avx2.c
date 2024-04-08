0118a847beb95ab65b3e2f159bdd1d0d19f76aca
siivonek <kari.siivonen@tuni.fi>
2024-04-08 14:15:24 +0300

    Implement w8 vertical 1 to 4 upsampling.

--- src/strategies/avx2/intra-avx2.c
+++ src/strategies/avx2/intra-avx2.c
@@ -6084,34 +6084,20 @@ static void mip_upsampling_w8_ups2_h16_ver_avx2(uvg_pixel* const dst, const uvg_
 
 static void mip_upsampling_w8_ups4_ver_avx2(uvg_pixel* const dst, const uvg_pixel* const src, const uvg_pixel* const ref)
 {
-  const uint8_t red_pred_size = 4;
+  const uint8_t red_pred_size = 8;
   const uint8_t ups_factor = 4; // height / red_pred_size
 
   const int log2_factor = uvg_g_convert_to_log2[ups_factor];
   const int rounding_offset = 1 << (log2_factor - 1);
-
   __m256i vrnd = _mm256_set1_epi16(rounding_offset);
 
-  /*__m128i vshufbefore = _mm_setr_epi8(
-    0xff, 0xff, 0xff, 0xff, 0x00, 0x01, 0x02, 0x03,
-    0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b
-  );*/
 
-  __m256i vshufres = _mm256_setr_epi8(
-    0x00, 0x01, 0x02, 0x03, 0x08, 0x09, 0x0a, 0x0b,
-    0x04, 0x05, 0x06, 0x07, 0x0c, 0x0d, 0x0e, 0x0f,
-    0x00, 0x01, 0x02, 0x03, 0x08, 0x09, 0x0a, 0x0b,
-    0x04, 0x05, 0x06, 0x07, 0x0c, 0x0d, 0x0e, 0x0f
-  );
-
-  int32_t refline = *(int32_t*)ref;
-  //__m128i vidx = _mm_setr_epi32(0, 32, 64, 96);
-  //__m128i vbehind = _mm_i32gather_epi32((const int*)src, vidx, 1);
+  int64_t refline = *(int64_t*)ref;
   __m128i vbehind = _mm_loadu_si128((__m128i*)src);
   __m128i vbefore = vbehind;
 
-  vbefore = _mm_slli_si128(vbefore, 4); // Shift left to make room for one 32-bit integer. This could be done with a shuffle, but there should be no performance difference.
-  vbefore = _mm_insert_epi32(vbefore, refline, 0);
+  vbefore = _mm_slli_si128(vbefore, 8); // Shift left to make room for one 64-bit integer. This could be done with a shuffle, but there should be no performance difference.
+  vbefore = _mm_insert_epi64(vbefore, refline, 0);
 
   __m256i vbefore256 = _mm256_cvtepu8_epi16(vbefore);
   __m256i vbehind256 = _mm256_cvtepu8_epi16(vbehind);
@@ -6127,10 +6113,6 @@ static void mip_upsampling_w8_ups4_ver_avx2(uvg_pixel* const dst, const uvg_pixe
   __m256i vrow1 = _mm256_add_epi16(vrow0, vinterpolate);
   __m256i vrow2 = _mm256_add_epi16(vrow1, vinterpolate);
 
-  /*vrow0 = _mm256_add_epi16(vrow0, vrnd);
-  vrow1 = _mm256_add_epi16(vrow1, vrnd);
-  vrow2 = _mm256_add_epi16(vrow2, vrnd);*/
-
   vrow0 = _mm256_srai_epi16(vrow0, log2_factor);
   vrow1 = _mm256_srai_epi16(vrow1, log2_factor);
   vrow2 = _mm256_srai_epi16(vrow2, log2_factor);
@@ -6138,18 +6120,42 @@ static void mip_upsampling_w8_ups4_ver_avx2(uvg_pixel* const dst, const uvg_pixe
   __m256i vres0 = _mm256_packus_epi16(vrow0, vrow1);
   __m256i vres1 = _mm256_packus_epi16(vrow2, vbehind256);
 
-  vres0 = _mm256_shuffle_epi8(vres0, vshufres);
-  vres1 = _mm256_shuffle_epi8(vres1, vshufres);
-
   __m256i vlo128 = _mm256_permute2x128_si256(vres0, vres1, 0x20);
   __m256i vhi128 = _mm256_permute2x128_si256(vres0, vres1, 0x31);
 
-  vres0 = _mm256_permute4x64_epi64(vlo128, _MM_SHUFFLE(3, 1, 2, 0));
-  vres1 = _mm256_permute4x64_epi64(vhi128, _MM_SHUFFLE(3, 1, 2, 0));
+  _mm256_store_si256((__m256i*)(dst + 0), vlo128);
+  _mm256_store_si256((__m256i*)(dst + 32), vhi128);
 
-  _mm256_store_si256((__m256i*)(dst + 0), vres0);
-  _mm256_store_si256((__m256i*)(dst + 32), vres1);
+  for (int i = 1; i < 4; ++i) {
+    vbefore = _mm_loadu_si128((__m128i*)(src + (i * 16 - 8)));
+    vbehind = _mm_loadu_si128((__m128i*)(src + (i * 16)));
+    vbefore256 = _mm256_cvtepu8_epi16(vbefore);
+    vbehind256 = _mm256_cvtepu8_epi16(vbehind);
 
+    vbeforeshifted = _mm256_slli_epi16(vbefore256, log2_factor);
+
+    // Add rounding offset
+    vbeforeshifted = _mm256_add_epi16(vbeforeshifted, vrnd);
+
+    vinterpolate = _mm256_sub_epi16(vbehind256, vbefore256);
+
+    vrow0 = _mm256_add_epi16(vbeforeshifted, vinterpolate);
+    vrow1 = _mm256_add_epi16(vrow0, vinterpolate);
+    vrow2 = _mm256_add_epi16(vrow1, vinterpolate);
+
+    vrow0 = _mm256_srai_epi16(vrow0, log2_factor);
+    vrow1 = _mm256_srai_epi16(vrow1, log2_factor);
+    vrow2 = _mm256_srai_epi16(vrow2, log2_factor);
+
+    vres0 = _mm256_packus_epi16(vrow0, vrow1);
+    vres1 = _mm256_packus_epi16(vrow2, vbehind256);
+
+    vlo128 = _mm256_permute2x128_si256(vres0, vres1, 0x20);
+    vhi128 = _mm256_permute2x128_si256(vres0, vres1, 0x31);
+
+    _mm256_store_si256((__m256i*)(dst + (i * 64) +  0), vlo128);
+    _mm256_store_si256((__m256i*)(dst + (i * 64) + 32), vhi128);
+  }
 }
 
 /** \brief Matrix weighted intra prediction.
@@ -6336,6 +6342,7 @@ void mip_predict_avx2(
           break;
         
         case 8: 
+          // TODO: remove the if clauses and add a switch for height
           if (ups_ver_factor == 2) {
             if (height == 8) {
               mip_upsampling_w8_ups2_h8_ver_avx2(result, ver_src, ref_samples_top);
