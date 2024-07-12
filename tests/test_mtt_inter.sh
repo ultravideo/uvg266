ddf9b7dc2b7aa4205b80a8e980d2a8f4d6ee14b2
Jaakko Laitinen <jaakko.laitinen@tuni.fi>
2024-07-12 13:56:34 +0300

    Add CI tests for inter mtt

---
+++ tests/test_mtt_inter.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Test all-intra coding.
+
+set -eu
+
+. "${0%/*}/util.sh"
+
+common_args='264x130 10 yuv420p -p1 --preset=ultrafast --threads=0 --no-cpuid --no-wpp --fast-residual-cost 0'
+#valgrind_test $common_args --rd=0 --mtt-depth-intra 1 --pu-depth-intra 2-3
+#valgrind_test $common_args --rd=3 --mtt-depth-intra 1 --pu-depth-intra 0-5
+#valgrind_test $common_args --rd=3 --mtt-depth-intra 3 --pu-depth-intra 0-8
+#valgrind_test $common_args --rd=3 --mtt-depth-intra 3 --mtt-depth-intra-chroma 3 --dual-tree --pu-depth-intra 0-8
+#valgrind_test $common_args --rd=3 --rdoq --jccr --isp --lfnst --mip --mrl --mts intra --cclm --mtt-depth-intra 3 --mtt-depth-intra-chroma 3 --dual-tree --pu-depth-intra 0-8
+
+valgrind_test $common_args --rd=0 --mtt-depth-inter 1 --pu-depth-inter 2-3
+valgrind_test $common_args --rd=3 --mtt-depth-inter 1 --pu-depth-inter 1-5
+valgrind_test $common_args --rd=3 --mtt-depth-inter 3 --pu-depth-inter 1-8
