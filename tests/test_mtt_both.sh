cf9f8ead13e5413d55dfd89d91548b5163f6c266
Jaakko Laitinen <jaakko.laitinen@tuni.fi>
2024-07-15 15:17:49 +0300

    Adjust parameters so that inter is actually used

--- tests/test_mtt_both.sh
+++ tests/test_mtt_both.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Test all-intra coding.
+# Test combined intra/inter mtt coding.
 
 set -eu
 
 . "${0%/*}/util.sh"
 
-common_args='264x130 10 yuv420p -p1 --preset=ultrafast --threads=0 --no-cpuid --no-wpp --fast-residual-cost 0'
+common_args='264x130 10 yuv420p --preset=ultrafast --threads=0 --no-cpuid --no-wpp --fast-residual-cost 0'
 valgrind_test $common_args --rd=0 --mtt-depth-intra 1 --pu-depth-intra 2-3 --mtt-depth-inter 1 --pu-depth-inter 2-3
 valgrind_test $common_args --rd=3 --mtt-depth-intra 1 --pu-depth-intra 0-5 --mtt-depth-inter 1 --pu-depth-inter 1-5
 valgrind_test $common_args --rd=3 --mtt-depth-intra 3 --pu-depth-intra 0-8 --mtt-depth-inter 3 --pu-depth-inter 1-8
