e8bc87c36c8da66cd07e4e97d452b2c3e52cfacf
Jaakko Laitinen <jaakko.laitinen@tuni.fi>
2024-10-09 15:39:46 +0300

    Update inter depth range to match intra

--- tests/test_mtt_both.sh
+++ tests/test_mtt_both.sh
@@ -8,7 +8,7 @@ set -eu
 
 common_args='264x130 10 yuv420p --preset=ultrafast --threads=0 --no-cpuid --no-wpp --fast-residual-cost 0'
 valgrind_test $common_args --rd=0 --mtt-depth-intra 1 --pu-depth-intra 2-3 --mtt-depth-inter 1 --pu-depth-inter 2-3
-valgrind_test $common_args --rd=3 --mtt-depth-intra 1 --pu-depth-intra 0-5 --mtt-depth-inter 1 --pu-depth-inter 1-5
-valgrind_test $common_args --rd=3 --mtt-depth-intra 3 --pu-depth-intra 0-8 --mtt-depth-inter 3 --pu-depth-inter 1-8
-valgrind_test $common_args --rd=3 --mtt-depth-intra 3 --mtt-depth-intra-chroma 3 --dual-tree --pu-depth-intra 0-8 --mtt-depth-inter 3 --pu-depth-inter 1-8
-valgrind_test $common_args --rd=3 --rdoq --jccr --isp --lfnst --mip --mrl --mts intra --cclm --mtt-depth-intra 3 --mtt-depth-intra-chroma 3 --dual-tree --pu-depth-intra 0-8 --mtt-depth-inter 3 --pu-depth-inter 1-8
+valgrind_test $common_args --rd=3 --mtt-depth-intra 1 --pu-depth-intra 0-5 --mtt-depth-inter 1 --pu-depth-inter 0-5
+valgrind_test $common_args --rd=3 --mtt-depth-intra 3 --pu-depth-intra 0-8 --mtt-depth-inter 3 --pu-depth-inter 0-8
+valgrind_test $common_args --rd=3 --mtt-depth-intra 3 --mtt-depth-intra-chroma 3 --dual-tree --pu-depth-intra 0-8 --mtt-depth-inter 3 --pu-depth-inter 0-8
+valgrind_test $common_args --rd=3 --rdoq --jccr --isp --lfnst --mip --mrl --mts intra --cclm --mtt-depth-intra 3 --mtt-depth-intra-chroma 3 --dual-tree --pu-depth-intra 0-8 --mtt-depth-inter 3 --pu-depth-inter 0-8
--- tests/test_mtt_inter.sh
+++ tests/test_mtt_inter.sh
@@ -9,5 +9,5 @@ set -eu
 common_args='264x130 10 yuv420p --preset=ultrafast --threads=0 --no-cpuid --no-wpp --fast-residual-cost 0'
 
 valgrind_test $common_args --rd=0 --mtt-depth-inter 1 --pu-depth-inter 2-3
-valgrind_test $common_args --rd=3 --mtt-depth-inter 1 --pu-depth-inter 1-5
-valgrind_test $common_args --rd=3 --mtt-depth-inter 3 --pu-depth-inter 1-8
+valgrind_test $common_args --rd=3 --mtt-depth-inter 1 --pu-depth-inter 0-5
+valgrind_test $common_args --rd=3 --mtt-depth-inter 3 --pu-depth-inter 0-8
